4aed519eff20e68ee2c699c2842af86c7fb6539d
Dan Ekström <dan@oakenstream.com>
2017-07-11 17:58:17 +0200

    Added css

--- js/simpleTypeAhead.js
+++ js/simpleTypeAhead.js
@@ -36,6 +36,22 @@ typeAhead.directive('typeahead', function($timeout) {
       onSelect: '&amp'
     },
     link: function(scope, elem, attrs) {
+      scope.handleSelection = function(selectedItem) {
+        scope.model = selectedItem;
+        scope.current = 0;
+        scope.selected = true;
+        $timeout(function() {
+          scope.onSelect();
+        }, 200);
+      };
+      scope.current = 0;
+      scope.selected = true; // hides the list initially
+      scope.isCurrent = function(index) {
+        return scope.current == index;
+      };
+      scope.setCurrent = function(index) {
+        scope.current = index;
+      };
     },
     templateUrl: 'template/template.html'
   };
